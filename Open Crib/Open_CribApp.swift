709ee56c8dd4e06e7e4b5faf81828a0ae7db2527
Wilson Narea <wilsonnarea27@gmail.com>
2021-12-15 21:15:36 -0500

    Initial Commit

---
+++ Open Crib/Open_CribApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_CribApp.swift
+//  Open Crib
+//
+//  Created by Wilson Narea on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Open_CribApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

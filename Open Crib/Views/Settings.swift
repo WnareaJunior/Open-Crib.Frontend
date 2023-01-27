14b3d42947e007c8daca3e06c9111153446d5ca3
Wilson Narea <wilsonnarea27@gmail.com>
2023-01-27 11:49:04 -0500

    Set to dark mode always

--- Open Crib/Views/Settings.swift
+++ Open Crib/Views/Settings.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Settings: View {
+    @State var itsOn = true
+    var body: some View {
+        
+        List{
+            Text("who")
+            Toggle("Private Account", isOn: $itsOn)
+        }
+    }
+}
+
+
+
+struct Settings_Preview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Settings()
+        }
+    }
+}

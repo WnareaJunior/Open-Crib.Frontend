5458545ba12e6ecf09ca7ea3e67db79557b82e93
Oscar Alvarez <67487448+oscaralvarez711@users.noreply.github.com>
2023-01-30 13:27:07 -0500

    profile followers following rank

--- Open Crib/Views/Profile.swift
+++ Open Crib/Views/Profile.swift
@@ -14,21 +14,58 @@ struct Profile: View {
         ZStack{
             Color.cribGray
                 .ignoresSafeArea()
-            Text("Profile")
-                .foregroundColor(Color.white)
-                .onTapGesture {
-                    do {
-                        try Auth.auth().signOut()
-                    } catch let signOutError as NSError {
-                      print("Error signing out: %@", signOutError)
+            VStack{
+                HStack{
+                    Circle()
+                    .frame(width: 130, height: 130, alignment: .leading)
+                    .padding(.trailing, 15)
+                    
+                    VStack{
+                        Text("300")
+                        .font(Font.custom("MADETOMMY-Bold", size: 25))
+                        Text("Followers")
+                        .font(Font.custom("MADETOMMY-Bold", size: 15))
+                        .foregroundColor(Color.cribCyan)
+                    }
+                    VStack{
+                        Text("300")
+                        .font(Font.custom("MADETOMMY-Bold", size: 25))
+                        Text("Following")
+                        .font(Font.custom("MADETOMMY-Bold", size: 15))
+                        .foregroundColor(Color.cribCyan)
+                    }
+                    VStack{
+                        Text("300")
+                        .font(Font.custom("MADETOMMY-Bold", size: 25))
+                        Text("Ranked")
+                        .font(Font.custom("MADETOMMY-Bold", size: 15))
+                        .foregroundColor(Color.cribCyan)
                     }
                 }
+            .frame(width: UIScreen.main.bounds.maxX, height: 230, alignment: .center)
+
+            }
+            .frame(width: UIScreen.main.bounds.maxX, height: UIScreen.main.bounds.maxY, alignment: .top)
+            
+            
+            
+            
+//            Text("Profile")
+//                .foregroundColor(Color.white)
+//                .onTapGesture {
+//                    do {
+//                        try Auth.auth().signOut()
+//                    } catch let signOutError as NSError {
+//                      print("Error signing out: %@", signOutError)
+//                    }
+//                }
+        
         }
     }
 }
 
 struct Profile_Previews: PreviewProvider {
     static var previews: some View {
-        Profile()
+            Profile()
+        }
     }
-}
